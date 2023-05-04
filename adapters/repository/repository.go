8ceaab133408b812789c55459398ad1e82a9edfd
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2023-05-04 21:26:22 +0700

    add code to publish message to cloud pub/sub

--- adapters/repository/repository.go
+++ adapters/repository/repository.go
@@ -7,28 +7,37 @@ import (
 	"os"
 
 	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/pubsub"
 	"github.com/joho/godotenv"
 )
 
 type BookFirestoreRepository struct {
 	client firestore.Client
+	pubsub pubsub.Client
 }
 
-func NewBookFirestoreRepository(ctx context.Context) *BookFirestoreRepository{
+func NewBookFirestoreRepository(ctx context.Context) *BookFirestoreRepository {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("[NewBookFirestoreRepository]error load .env file with error %v \n", err)
 	}
 
 	projectId := os.Getenv("GCP_PROJECT_ID")
+	log.Println(projectId)
 	client, err := firestore.NewClient(ctx, projectId)
-
 	if err != nil {
-		log.Printf("NewBookFirestoreRepository error with %v \n", err)
+		log.Fatalf("[NewBookFirestoreRepository] failed to initialize firestore client with error %v \n", err)
+		return nil
+	}
+	pubsubClient, err := pubsub.NewClient(context.Background(), projectId)
+	if err != nil {
+		log.Fatalf("[NewBookFirestoreRepository] failed to initialize pubsub client with error %v \n", err)
+		return nil
 	}
 
 	return &BookFirestoreRepository{
 		client: *client,
+		pubsub: *pubsubClient,
 	}
 }
 
@@ -41,5 +50,14 @@ func (b *BookFirestoreRepository) AddBook(book domain.Book) error {
 		return err
 	}
 
+	topic := b.pubsub.Topic("upload-image")
+	res := topic.Publish(ctx, &pubsub.Message{
+		Data: []byte("book added"),
+	})
+	_, err = res.Get(ctx)
+	if err != nil {
+		log.Printf("[BookFirestoreRepository.AddBook] fail to publish message to %v topic with error %v \n", topic, err)
+		return err
+	}
 	return nil
 }
